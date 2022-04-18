bb8be908c9546b768dbd98d286f6e56e28576a75
mbejadiana <di.mbeja@gmail.com>
2022-04-18 03:52:24 -0700

    correct changes

--- _printf.c
+++ _printf.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
+	register short len = 0;
+	int (*printFunc)(va_list, mods *);
+	mods prefixes = PF_INIT;
+	const char *p = format;
 	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	va_start(arguments, format);
+	assert(invalidInputs(p));
+	for (; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+			p++;
+			if (*p == '%')
+			{
+				len += _putchar('%');
+				continue;
 			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+			while (get_flags(*p, &prefixes))
+				p++;
+			printFunc = get_print(*p);
+			len += (printFunc)
+				? printFunc(arguments, &prefixes)
+				: _printf("%%%c", *p);
+		} else
+			len += _putchar(*p);
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	_putchar(FLUSH);
+	va_end(arguments);
 	return (len);
 }
--- main.h
+++ main.h
@@ -1,61 +1,97 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdarg.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <stdbool.h>
+
+/* macros */
+#define FLUSH -1
+#define PF_INIT {false, false, false}
+#define NIL "(nil)"
+#define HEXA "0x"
+#define NULL_STRING "(null)"
+#define NUL '\0'
+#define KILOBYTE 1024
+#define ABS(x) (((x) <  0) ? -(x) : (x))
 
 /**
- * struct print - struct for printer functions
- * @type_arg: identifier
- * @f: pointer to a printer functions
- *
- * Description: struct that stores pointers to a
- * printer functions
+ * struct modifiers - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
-typedef struct print
+typedef struct modifiers
 {
-	char *type_arg;
-	int (*f)(va_list, char *, unsigned int);
-} print_t;
+	_Bool plus;
+	_Bool space;
+	_Bool hash;
+} mods;
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, mods *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, mods *f);
+void print_number(int n);
+int print_unsigned(va_list l, mods *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, mods *f);
+int print_hex_big(va_list l, mods *f);
+int print_binary(va_list l, mods *f);
+int print_octal(va_list l, mods *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
-int print_prg(va_list __attribute__((unused)), char *, unsigned int);
-int print_chr(va_list arguments, char *buf, unsigned int ibuf);
-int print_str(va_list arguments, char *buf, unsigned int ibuf);
-int print_int(va_list arguments, char *buf, unsigned int ibuf);
-int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
-int print_unt(va_list arguments, char *buf, unsigned int ibuf);
-int print_oct(va_list arguments, char *buf, unsigned int ibuf);
-int print_hex(va_list arguments, char *buf, unsigned int ibuf);
-int print_upx(va_list arguments, char *buf, unsigned int ibuf);
-int print_usr(va_list arguments, char *buf, unsigned int ibuf);
-int print_add(va_list arguments, char *buf, unsigned int ibuf);
-int print_rev(va_list arguments, char *buf, unsigned int ibuf);
-int print_rot(va_list arguments, char *buf, unsigned int ibuf);
-int prinlint(va_list arguments, char *buf, unsigned int ibuf);
-int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinloct(va_list arguments, char *buf, unsigned int ibuf);
-int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinhint(va_list arguments, char *buf, unsigned int ibuf);
-int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
-int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinpint(va_list arguments, char *buf, unsigned int ibuf);
-int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
-int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
-int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
-int prinsint(va_list arguments, char *buf, unsigned int ibuf);
-int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *fill_oct_array(char *bnr, char *oct);
-char *fill_long_oct_array(char *bnr, char *oct);
-char *fill_short_oct_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+
+/* get_print */
+int (*get_print(char s))(va_list, mods *);
+
+/* get_flags */
+int get_flags(char s, mods *f);
+
+/* print_alpha */
+int print_string(va_list l, mods *f);
+int print_char(va_list l, mods *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, mods *f);
+int print_rev(va_list l, mods *f);
+int print_bigS(va_list l, mods *f);
+_Bool isNonAlphaNumeric(char c);
+
+/* print_address */
+int print_address(va_list l, mods *f);
+
+/* print_percent */
+int print_percent(va_list l, mods *f);
+
+/* assertions */
+_Bool invalidInputs(const char *p);
+
+/* unit_tests */
+void tusBasicTester(void);
+void tusAdvancedTester(void);
+void tusSuperAdvanced(void);
 
 #endif
